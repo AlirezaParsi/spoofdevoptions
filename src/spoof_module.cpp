9bbf96d6b4d3bbec13993675fafce23dc52dcbd9
Aliza <alirezanajmabadi79@gmail.com>
2025-08-17 18:45:47 +0330

    Update spoof_module.cpp

--- src/spoof_module.cpp
+++ src/spoof_module.cpp
@@ -1,3 +1,4 @@
+// src/monitor_module.cpp
 #include <jni.h>
 #include <string>
 #include <android/log.h>
@@ -5,137 +6,93 @@
 
 #define LOG_TAG "SpoofModule"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define ARRAYSIZE(a) (sizeof(a)/sizeof((a)[0]))
 
-// ------------------------- SystemProperties -------------------------
-static jstring (*orig_SP_get1)(JNIEnv*, jclass, jstring);
-static jstring (*orig_SP_get2)(JNIEnv*, jclass, jstring, jstring);
-static jboolean (*orig_SP_getBoolean)(JNIEnv*, jclass, jstring, jboolean);
-static jint (*orig_SP_getInt)(JNIEnv*, jclass, jstring, jint);
-static jlong (*orig_SP_getLong)(JNIEnv*, jclass, jstring, jlong);
+// ------------------ SystemProperties (native_*) ------------------
+static jstring  (*orig_SP_native_get)(JNIEnv*, jclass, jstring);
+static jint     (*orig_SP_native_get_int)(JNIEnv*, jclass, jstring, jint);
+static jlong    (*orig_SP_native_get_long)(JNIEnv*, jclass, jstring, jlong);
+static jboolean (*orig_SP_native_get_boolean)(JNIEnv*, jclass, jstring, jboolean);
 
-static jstring hook_SP_get1(JNIEnv* env, jclass clazz, jstring key) {
-    const char* k = env->GetStringUTFChars(key, nullptr);
-    LOGD("SystemProperties.get(String) key=%s", k);
-    env->ReleaseStringUTFChars(key, k);
-
-    return orig_SP_get1 ? orig_SP_get1(env, clazz, key) : nullptr;
+static jstring hook_SP_native_get(JNIEnv* env, jclass clazz, jstring key) {
+    const char* k = key ? env->GetStringUTFChars(key, nullptr) : "";
+    LOGD("SystemProperties.native_get key=%s", k);
+    if (key) env->ReleaseStringUTFChars(key, k);
+    return orig_SP_native_get ? orig_SP_native_get(env, clazz, key) : nullptr;
+}
+static jint hook_SP_native_get_int(JNIEnv* env, jclass clazz, jstring key, jint def) {
+    const char* k = key ? env->GetStringUTFChars(key, nullptr) : "";
+    LOGD("SystemProperties.native_get_int key=%s def=%d", k, (int)def);
+    if (key) env->ReleaseStringUTFChars(key, k);
+    return orig_SP_native_get_int ? orig_SP_native_get_int(env, clazz, key, def) : def;
+}
+static jlong hook_SP_native_get_long(JNIEnv* env, jclass clazz, jstring key, jlong def) {
+    const char* k = key ? env->GetStringUTFChars(key, nullptr) : "";
+    LOGD("SystemProperties.native_get_long key=%s def=%lld", k, (long long)def);
+    if (key) env->ReleaseStringUTFChars(key, k);
+    return orig_SP_native_get_long ? orig_SP_native_get_long(env, clazz, key, def) : def;
+}
+static jboolean hook_SP_native_get_boolean(JNIEnv* env, jclass clazz, jstring key, jboolean def) {
+    const char* k = key ? env->GetStringUTFChars(key, nullptr) : "";
+    LOGD("SystemProperties.native_get_boolean key=%s def=%d", k, (int)def);
+    if (key) env->ReleaseStringUTFChars(key, k);
+    return orig_SP_native_get_boolean ? orig_SP_native_get_boolean(env, clazz, key, def) : def;
 }
 
-static jstring hook_SP_get2(JNIEnv* env, jclass clazz, jstring key, jstring def) {
-    const char* k = env->GetStringUTFChars(key, nullptr);
-    LOGD("SystemProperties.get(String,String) key=%s", k);
-    env->ReleaseStringUTFChars(key, k);
+// ------------------ Settings.getStringForUser ------------------
+static jstring (*orig_getStringForUser)(JNIEnv*, jclass, jobject, jstring, jint);
 
-    return orig_SP_get2 ? orig_SP_get2(env, clazz, key, def) : def;
+static jstring hook_getStringForUser(JNIEnv* env, jclass clazz, jobject cr, jstring name, jint user) {
+    const char* n = name ? env->GetStringUTFChars(name, nullptr) : "";
+    LOGD("Settings.getStringForUser key=%s user=%d", n, (int)user);
+    if (name) env->ReleaseStringUTFChars(name, n);
+    return orig_getStringForUser ? orig_getStringForUser(env, clazz, cr, name, user) : nullptr;
 }
 
-static jboolean hook_SP_getBoolean(JNIEnv* env, jclass clazz, jstring key, jboolean def) {
-    const char* k = env->GetStringUTFChars(key, nullptr);
-    LOGD("SystemProperties.getBoolean key=%s", k);
-    env->ReleaseStringUTFChars(key, k);
-
-    return orig_SP_getBoolean ? orig_SP_getBoolean(env, clazz, key, def) : def;
+static void hook_settings_class(zygisk::Api* api, JNIEnv* env, const char* cls) {
+    JNINativeMethod m[] = {
+        {"getStringForUser", "(Landroid/content/ContentResolver;Ljava/lang/String;I)Ljava/lang/String;", (void*) hook_getStringForUser},
+    };
+    api->hookJniNativeMethods(env, cls, m, ARRAYSIZE(m));
+    LOGD("Hooked %s.getStringForUser -> orig=%p", cls, (void*)m[0].fnPtr);
+    if (!orig_getStringForUser) {
+        orig_getStringForUser = (decltype(orig_getStringForUser)) m[0].fnPtr;
+    }
 }
 
-static jint hook_SP_getInt(JNIEnv* env, jclass clazz, jstring key, jint def) {
-    const char* k = env->GetStringUTFChars(key, nullptr);
-    LOGD("SystemProperties.getInt key=%s", k);
-    env->ReleaseStringUTFChars(key, k);
-
-    return orig_SP_getInt ? orig_SP_getInt(env, clazz, key, def) : def;
-}
-
-static jlong hook_SP_getLong(JNIEnv* env, jclass clazz, jstring key, jlong def) {
-    const char* k = env->GetStringUTFChars(key, nullptr);
-    LOGD("SystemProperties.getLong key=%s", k);
-    env->ReleaseStringUTFChars(key, k);
-
-    return orig_SP_getLong ? orig_SP_getLong(env, clazz, key, def) : def;
-}
-
-// ------------------------- Settings -------------------------
-static jint (*orig_Settings_getInt1)(JNIEnv*, jclass, jobject, jstring);
-static jint (*orig_Settings_getInt2)(JNIEnv*, jclass, jobject, jstring, jint);
-static jstring (*orig_Settings_getStringForUser)(JNIEnv*, jclass, jobject, jstring, jint);
-
-static jint hook_Settings_getInt1(JNIEnv* env, jclass clazz, jobject cr, jstring name) {
-    const char* k = env->GetStringUTFChars(name, nullptr);
-    LOGD("Settings.getInt(cr,key) key=%s", k);
-    env->ReleaseStringUTFChars(name, k);
-
-    return orig_Settings_getInt1 ? orig_Settings_getInt1(env, clazz, cr, name) : 0;
-}
-
-static jint hook_Settings_getInt2(JNIEnv* env, jclass clazz, jobject cr, jstring name, jint def) {
-    const char* k = env->GetStringUTFChars(name, nullptr);
-    LOGD("Settings.getInt(cr,key,def) key=%s", k);
-    env->ReleaseStringUTFChars(name, k);
-
-    return orig_Settings_getInt2 ? orig_Settings_getInt2(env, clazz, cr, name, def) : def;
-}
-
-static jstring hook_Settings_getStringForUser(JNIEnv* env, jclass clazz, jobject cr, jstring name, jint user) {
-    const char* k = env->GetStringUTFChars(name, nullptr);
-    LOGD("Settings.getStringForUser key=%s user=%d", k, user);
-    env->ReleaseStringUTFChars(name, k);
-
-    return orig_Settings_getStringForUser ? orig_Settings_getStringForUser(env, clazz, cr, name, user) : nullptr;
-}
-
-// ------------------------- Module -------------------------
-class SpoofModule : public zygisk::ModuleBase {
+class MonitorModule : public zygisk::ModuleBase {
 public:
     void onLoad(zygisk::Api* api, JNIEnv* env) override {
-        this->api = api;
-        this->env = env;
+        this->api = api; this->env = env;
     }
 
     void preAppSpecialize(zygisk::AppSpecializeArgs* args) override {
         const char* pkg = args->nice_name ? env->GetStringUTFChars(args->nice_name, nullptr) : "";
-        LOGD("App detected: %s", pkg);
+        LOGD("App detected: %s", pkg ? pkg : "(null)");
 
-        // فقط اگه خواستی روی یک اپ خاص فعال بشه، شرط بگذار:
-        // if (!pkg || std::string(pkg) != "com.example.app") return;
-
-        // ---- Hook SystemProperties ----
+        // --- SystemProperties native_* ---
         {
             JNINativeMethod m[] = {
-                {"get", "(Ljava/lang/String;)Ljava/lang/String;", (void*) hook_SP_get1},
-                {"get", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;", (void*) hook_SP_get2},
-                {"getBoolean", "(Ljava/lang/String;Z)Z", (void*) hook_SP_getBoolean},
-                {"getInt", "(Ljava/lang/String;I)I", (void*) hook_SP_getInt},
-                {"getLong", "(Ljava/lang/String;J)J", (void*) hook_SP_getLong},
+                {"native_get", "(Ljava/lang/String;)Ljava/lang/String;", (void*) hook_SP_native_get},
+                {"native_get_int", "(Ljava/lang/String;I)I", (void*) hook_SP_native_get_int},
+                {"native_get_long", "(Ljava/lang/String;J)J", (void*) hook_SP_native_get_long},
+                {"native_get_boolean", "(Ljava/lang/String;Z)Z", (void*) hook_SP_native_get_boolean},
             };
-            api->hookJniNativeMethods(env, "android/os/SystemProperties", m, 5);
-            orig_SP_get1 = (decltype(orig_SP_get1)) m[0].fnPtr;
-            orig_SP_get2 = (decltype(orig_SP_get2)) m[1].fnPtr;
-            orig_SP_getBoolean = (decltype(orig_SP_getBoolean)) m[2].fnPtr;
-            orig_SP_getInt = (decltype(orig_SP_getInt)) m[3].fnPtr;
-            orig_SP_getLong = (decltype(orig_SP_getLong)) m[4].fnPtr;
+            api->hookJniNativeMethods(env, "android/os/SystemProperties", m, ARRAYSIZE(m));
+            orig_SP_native_get          = (decltype(orig_SP_native_get)) m[0].fnPtr;
+            orig_SP_native_get_int      = (decltype(orig_SP_native_get_int)) m[1].fnPtr;
+            orig_SP_native_get_long     = (decltype(orig_SP_native_get_long)) m[2].fnPtr;
+            orig_SP_native_get_boolean  = (decltype(orig_SP_native_get_boolean)) m[3].fnPtr;
+            LOGD("SP.native_get ptrs: get=%p get_int=%p get_long=%p get_bool=%p",
+                 (void*)orig_SP_native_get, (void*)orig_SP_native_get_int,
+                 (void*)orig_SP_native_get_long, (void*)orig_SP_native_get_boolean);
         }
 
-        // ---- Hook Settings.Global ----
-        {
-            JNINativeMethod m[] = {
-                {"getInt", "(Landroid/content/ContentResolver;Ljava/lang/String;)I", (void*) hook_Settings_getInt1},
-                {"getInt", "(Landroid/content/ContentResolver;Ljava/lang/String;I)I", (void*) hook_Settings_getInt2},
-                {"getStringForUser", "(Landroid/content/ContentResolver;Ljava/lang/String;I)Ljava/lang/String;", (void*) hook_Settings_getStringForUser},
-            };
-            api->hookJniNativeMethods(env, "android/provider/Settings$Global", m, 3);
-            orig_Settings_getInt1 = (decltype(orig_Settings_getInt1)) m[0].fnPtr;
-            orig_Settings_getInt2 = (decltype(orig_Settings_getInt2)) m[1].fnPtr;
-            orig_Settings_getStringForUser = (decltype(orig_Settings_getStringForUser)) m[2].fnPtr;
-        }
-
-        // ---- Hook Settings.Secure ----
-        {
-            JNINativeMethod m[] = {
-                {"getInt", "(Landroid/content/ContentResolver;Ljava/lang/String;)I", (void*) hook_Settings_getInt1},
-                {"getInt", "(Landroid/content/ContentResolver;Ljava/lang/String;I)I", (void*) hook_Settings_getInt2},
-                {"getStringForUser", "(Landroid/content/ContentResolver;Ljava/lang/String;I)Ljava/lang/String;", (void*) hook_Settings_getStringForUser},
-            };
-            api->hookJniNativeMethods(env, "android/provider/Settings$Secure", m, 3);
-        }
+        // --- Settings.*.getStringForUser on multiple classes ---
+        hook_settings_class(api, env, "android/provider/Settings$Secure");
+        hook_settings_class(api, env, "android/provider/Settings$System");
+        hook_settings_class(api, env, "android/provider/Settings$Global");
+        hook_settings_class(api, env, "android/provider/Settings$NameValueCache");
 
         if (pkg) env->ReleaseStringUTFChars(args->nice_name, pkg);
     }
@@ -145,4 +102,4 @@ private:
     JNIEnv* env{};
 };
 
-REGISTER_ZYGISK_MODULE(SpoofModule)
+REGISTER_ZYGISK_MODULE(MonitorModule)
